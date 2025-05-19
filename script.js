9170c07aa2881d97dbbcca9a55fc2e97f6317927
Marco-Affonso <marco_affonso@hotmail.com>
2025-05-19 12:52:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const div = document.getElementById("mensagem");
+    div.innerText = "Bem-vindo à minha página! Continue acreditando nos seus sonhos e lutando por eles!";
+}
